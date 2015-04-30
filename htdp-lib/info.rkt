8c27d05aeab7a311081b1cb1b410e6c1e2960a5d
Sam Tobin-Hochstadt <samth@cs.indiana.edu>
2015-04-30 15:46:31 -0400

    Reduce dependencies based on drracket/deinprogramm changes.

--- htdp-lib/info.rkt
+++ htdp-lib/info.rkt
@@ -3,36 +3,36 @@
 (define collection 'multi)
 
 (define deps
-  '("scheme-lib"
-    "srfi-lite-lib"
-    "base"
-    "compatibility-lib"
+  '("plai"
+    "deinprogramm-signature"
     "draw-lib"
+    ("drracket-plugin-lib" #:version "1.1")
     "errortrace-lib"
-    ("gui-lib" #:version "1.7")
     "html-lib"
-    "images-lib"
     "images-gui-lib"
+    "images-lib"
     "net-lib"
-    "plai"
-    "pconvert-lib"
     "r5rs-lib"
     "sandbox-lib"
     "scribble-lib"
     "slideshow-lib"
-    "snip-lib"    
-    "string-constants-lib"
     "typed-racket-lib"
-    "web-server-lib"
     "wxme-lib"
-    "drracket"
-    "deinprogramm"))
-(define build-deps '("racket-index"
-                     "at-exp-lib"
-                     "rackunit-lib"))
+    "pconvert-lib"
+    "snip-lib"
+    "string-constants-lib"
+    "web-server-lib"
+    "scheme-lib"
+    "srfi-lite-lib"
+    "base"
+    "compatibility-lib"
+    ("gui-lib" #:version "1.7")))
 
 (define pkg-desc "implementation (no documentation) part of \"htdp\"")
 
 (define pkg-authors '(matthias mflatt robby))
 
 (define version "1.3")
+(define build-deps '("at-exp-lib"
+                     "racket-index"
+                     "rackunit-lib"))
